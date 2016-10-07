4f623cd61ef52076fe79cb4fb305311406fd3805
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2016-10-07 10:44:34 +0200

    Add #respond_with_errors helper to FormHelper

--- lib/ons-context/util/form_helper.rb
+++ lib/ons-context/util/form_helper.rb
@@ -14,6 +14,13 @@ module OnsContext
       def clean_params
         object_params
       end
+
+      def respond_with_errors(object_with_errors)
+        raise ArgumentError unless object_with_errors.respond_to?(:errors)
+
+        response = object_with_errors.errors.to_hash(true)
+        render json: response, status: :unprocessable_entity
+      end
     end
   end
 end
