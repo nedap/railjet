b14deadc6c3eab6c3b1606964d6d26eb61291fc2
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2016-11-17 14:39:48 +0100

    Allow to set different status while using #respond_with_errors

--- lib/railjet/util/form_helper.rb
+++ lib/railjet/util/form_helper.rb
@@ -15,11 +15,11 @@ module Railjet
         object_params
       end
 
-      def respond_with_errors(object_with_errors)
+      def respond_with_errors(object_with_errors, status: :unprocessable_entity)
         raise ArgumentError unless object_with_errors.respond_to?(:errors)
 
         response = object_with_errors.errors.to_hash(true)
-        render json: response, status: :unprocessable_entity
+        render json: response, status: status
       end
     end
   end
