e8d8b1e4583ee38e56a5e51ec6a7b0b1bf2077bd
Stephan Roolvink <stephan.roolvink@nedap.com>
2017-01-09 09:10:40 +0100

    Simplifies repository in attempt to fix memory_leak

--- lib/railjet/repository.rb
+++ lib/railjet/repository.rb
@@ -8,53 +8,33 @@ module Railjet
     attr_reader :registry
     delegate    :settings, to: :registry
 
-    def initialize(registry, **kwargs)
+    def initialize(registry, sources = {})
       @registry = registry
-      define_accessors(**kwargs)
-
-      initialize_record_repository if defined?(record_repository_class)
-      initialize_cupido_repository if defined?(cupido_repository_class)
+      @sources  = sources
     end
 
     private
 
-    def define_accessors(**kwargs)
-      kwargs.each do |name, val|
-        repository_module.send(:define_method, name) { val }
-        repository_module.send(:protected, name)
+    def record
+      if defined?(record_repository_class)
+        @record ||= record_repository_class.new(
+          registry, 
+          @sources[:record]
+        )
       end
     end
 
-    def repository_module
-      @repository_module ||= Module.new.tap { |m| self.class.include(m) }
-    end
-
-    def initialize_record_repository
-      if respond_to?(:record, true)
-        def self.record
-          @record ||= record_repository_class.new(registry, super)
-        end
-      else
-        def self.record
-          @record ||= record_repository_class.new(registry)
-        end
+    def cupido
+      if defined?(cupido_repository_class)
+        @cupido ||= cupido_repository_class.new(
+          registry,
+          @sources[:cupido]
+        )
       end
     end
 
     def record_repository_class
       self.class::ActiveRecordRepository
-    end
-
-    def initialize_cupido_repository
-      if respond_to?(:cupido, true)
-        def self.cupido
-          @cupido ||= cupido_repository_class.new(registry, super)
-        end
-      else
-        def self.cupido
-          @cupido ||= cupido_repository_class.new(registry)
-        end
-      end
     end
 
     def cupido_repository_class
